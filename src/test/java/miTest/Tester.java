7dd4fcbccf13f07ddbace3740c4e1246b49c511f
carvillav <charles_alex20@hotmail.com>
2020-04-22 16:29:19 -0300

    creación de métodos verSiTieneMayusculas, verSiTieneNumeros y verSiTieneOchoLetras, además de test para verificarlo

--- src/test/java/miTest/Tester.java
+++ src/test/java/miTest/Tester.java
@@ -10,4 +10,31 @@ public class Tester {
         Assert.assertEquals(1,1);
 
     }
+
+    @Test
+    public void verSiTieneMayuscula(){
+        String password = "hola";
+
+        char clave;
+        int contador = 0;
+        for (byte i = 0; i < password.length(); i++) {
+            clave = password.charAt(i);
+            String passValue = String.valueOf(clave);
+            if (passValue.matches("[A-Z]")) {
+                contador++;
+            }
+        }
+        if(contador > 0){
+            System.out.println("Tiene al menos una mayuscula");
+        }else{
+        System.out.println("NO tiene mayuscula");}
+    }
+
+    @Test
+    public void cantidadDeLetras(){
+        String palabra = "Hola123";
+        if(palabra.length()>=8){
+            System.out.println("Tiene al menos 8 caracteres");
+        }else{System.out.println("No tiene 8 caracteres");}
+    }
 }
