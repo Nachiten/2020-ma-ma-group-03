0fbbcd069703834c290e50ca5c76def2d63b578c
Ignacio Baptista [Notebook] <ibaptista@est.frba.utn.edu.ar>
2020-05-18 17:44:44 -0300

    Revert "Borrado proyecto de master"
    This reverts commit 9aa9d1a5f273d6a889470f40eab1ed893e4ecbe2.

---
+++ src/main/java/miPaquete/Principal.java
@@ -0,0 +1,12 @@
+package miPaquete;
+
+public class Principal {
+
+    public static void main(String[ ] args) {
+        ValidadorCredenciales.pedirPassword();
+
+        // Exit | 0 = Salió por aca
+    }
+}
+
+
---
+++ src/main/java/miPaquete/Usuario.java
@@ -0,0 +1,20 @@
+package miPaquete;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+// IDEA PARA LOS USUARIOS | NO IMPLEMENTADA EN NINGUN LADO By: Nachiten
+public abstract class Usuario {
+
+    //String user; // Para mas adelante
+    String password;
+
+    // Se inicia poniendo la contraseña establecida al crear el usuario. Se agregaran mas contrasenias a medida que se vayan cambiando.
+    List<String> contrasAnteriores = new ArrayList<String>();
+
+    // Este tiempo se establece al crear el usuario en la hora actual del sistema (la hora en la que se creó el usuario)
+    LocalDateTime tiempoUltimaContrasenia;
+
+}
+
