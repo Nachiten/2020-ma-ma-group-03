153d0b82191cb709c3e6ce4c8c8d5330e4912c0c
carvillav <charles_alex20@hotmail.com>
2020-10-19 13:30:56 -0300

    Clases router y server, se levanta servidor en el puerto 9000

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,8 +1,14 @@
 package server;
 
 
+import spark.Spark;
+import spark.debug.DebugScreen;
+
 public class Server {
     public static void main(String[] args) {
-
+        Spark.port(9000);
+        Router.init();
+        DebugScreen.enableDebugScreen();
+        System.out.println("Estas en el puerto 9000");
     }
 }
