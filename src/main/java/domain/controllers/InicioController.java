d9fc4d83a7c792cd587633e0d8cce66715419b80
noeliaescalier <noesc@est.frba.utn.edu.ar>
2020-10-21 17:54:31 -0300

    Creacion de paginas hbs

--- src/main/java/domain/controllers/InicioController.java
+++ src/main/java/domain/controllers/InicioController.java
@@ -15,51 +15,26 @@ import java.util.Map;
 public class InicioController {
 
     private AdministradorDeSesion administradorDeSesion;
+    /*private List<Usuario> usuarios;
 
-    private List<Usuario> usuarios;
 
     public InicioController() {
         usuarios = new ArrayList<>();
         administradorDeSesion = new AdministradorDeSesion();
-        unUsuarioController = new UsuarioController();
-    }
+    }*/
 
     public ModelAndView inicio(Request request, Response response) {
         administradorDeSesion.cerrarSesion(request);
         return new ModelAndView(null, "login.hbs");
-    }
-
-    public ModelAndView principal(Request request, Response response) {
-        return new ModelAndView(null, "principal.hbs");
     }
 /*
     public ModelAndView inicio(Request request, Response response){
         Map<String, Object> parametros = new HashMap<>();
         return new ModelAndView(parametros,"login.hbs");
     }*/
-
-    UsuarioController unUsuarioController;
-
     public Response login(Request request, Response response){
-            usuarios = this.unUsuarioController.getRepoUsuarios().buscarTodos();
-
-            String nombreDeUsuario = request.queryParams("nombreDeUsuario");
-            String contrasenia     = request.queryParams("contrasenia");
-
-            for ( Usuario unUsuario : usuarios ) {
-                if (unUsuario.getNombreUsuario().equals(nombreDeUsuario) &&  unUsuario.getContrasenia().equals(contrasenia)){
-                    System.out.println("Si encontre el usuario");
-                    response.redirect("/principal");
-                    return response;
-                }
-            }
-
-            System.out.println("No encontre el usuario");
-            response.redirect("/loginIncorrecto");
-            return response;
-        /*
         try{
-        RepositorioDeUsuarios repoUsuarios = FactoryRepositorioUsuarios.get();
+            RepositorioDeUsuarios repoUsuarios = FactoryRepositorioUsuarios.get();
 
             String nombreDeUsuario = request.queryParams("nombreDeUsuario");
             String contrasenia     = request.queryParams("contrasenia");
@@ -70,22 +45,19 @@ public class InicioController {
                 request.session(true);
                 request.session().attribute("id", usuario.getId());
 
-                System.out.println("Si encontre el usuario");
-                response.redirect("/principal");
+                response.redirect("/usuarios");
             }
             else{
-                System.out.println("No encontre el usuario");
-                response.redirect("/loginIncorrecto");
+                response.redirect("/");
             }
         }
         catch (Exception e){
             //Funcionalidad disponible solo con persistencia en Base de Datos
-            System.out.println("Mensaje de Excepcion: " + e.getMessage());
-            response.redirect("/loginIncorrecto");
+            response.redirect("/usuarios");
         }
         finally {
             return response;
-        }*/
+        }
     }
 
     public Response logout(Request request, Response response){
