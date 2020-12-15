101284c818dcfdc54032d43eafe560a94d24ff86
Ignacio Baptista [PC] <ibaptista@est.frba.utn.edu.ar>
2020-12-15 14:58:10 -0300

    Agregados archivos cargarDBCompletaHeroku.sql y eliminarDBCompletaHeroku.sql para generar y eliminar la db completa en heroku

---
+++ eliminarDBCompletaHeroku.sql
@@ -0,0 +1,56 @@
+-- Tablas Intermedias
+DROP TABLE criterio_categoriacriterio;
+DROP TABLE entidadjuridica_operaciondeegreso;
+DROP TABLE entidadjuridica_operaciondeingreso;
+DROP TABLE operaciondeegreso_categoriacriterio;
+DROP TABLE operaciondeegreso_item;
+DROP TABLE operaciondeegreso_presupuesto;
+DROP TABLE operaciondeegreso_usuario;
+DROP TABLE operaciondeingreso_operaciondeegreso;
+DROP TABLE presupuesto_categoriacriterio;
+DROP TABLE presupuesto_item;
+DROP TABLE usuario_operaciondeegreso;
+
+-- Criterios
+DROP TABLE categoriacriterio;
+DROP TABLE criterio;
+
+-- Otros
+DROP TABLE entidadbase;
+DROP TABLE item;
+DROP TABLE mensaje;
+DROP TABLE contraanterior;
+DROP TABLE presupuesto;
+
+-- Operaciones
+DROP TABLE operaciondeegreso;
+DROP TABLE operaciondeingreso;
+DROP TABLE criterioproveedormenorvalor;
+DROP TABLE proveedor;
+
+-- Documento comercial
+DROP TABLE documentocomercial;
+DROP TABLE tipodocumentocomercial;
+
+-- Medio pago
+DROP TABLE mediodepago;
+DROP TABLE tipomediodepago;
+
+-- Tipo entidad juridica
+DROP TABLE tipoentidadjuridicaempresa;
+DROP TABLE tipoentidadjuridicaorganizacionsectorsocial;
+DROP TABLE categoriaempresa;
+DROP TABLE sector;
+DROP TABLE usuario;
+DROP TABLE entidadjuridica;
+
+-- Direccion postal
+DROP TABLE direccionpostal;
+DROP TABLE direccion;
+DROP TABLE ciudades;
+DROP TABLE provincias;
+DROP TABLE paises;
+DROP TABLE monedas;
+
+-- Tabla de hibernate
+DROP TABLE hibernate_sequences
